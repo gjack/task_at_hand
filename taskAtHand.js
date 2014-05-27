b235e3ea731c9f9181a31b44b45839fe3e08a8c8
Gabriela Jack <gabriela@fuery.com>
2014-05-26 21:51:13 -0700

    making task names editable

--- taskAtHand.js
+++ taskAtHand.js
@@ -25,15 +25,31 @@ function TaskAtHandApp() {
     }
     
     function addTaskElement(taskName) {
-        var $task = $("<li></li>");
-        var $delete = $("<button class='delete'>X</button>");
-        var $moveUp = $("<button class='move-up'>^</button>");
-        var $moveDown = $("<button class='move-down'>v</button>");
-        $task.append($delete).append($moveUp).append($moveDown).append("<span class='task-name'>" + taskName + "</span>");
-        $delete.click(function(){$task.remove();});
-        $moveUp.click(function(){$task.insertBefore($task.prev());});
-        $moveDown.click(function(){$task.insertAfter($task.next());});
+        var $task = $("#task-template .task").clone();
+        $("span.task-name", $task).text(taskName);
         $("#task-list").append($task);
+        
+        $("button.delete", $task).click(function(){$task.remove();});
+        $("button.move-up", $task).click(function(){$task.insertBefore($task.prev());});
+        $("button.move-down", $task).click(function(){$task.insertAfter($task.next());});
+        $("span.task-name", $task).click(function(){
+            onEditTaskName($(this));});
+        $("input.task-name", $task).change(function(){
+            onChangeTaskName($(this));}).blur(function(){
+            $(this).hide().siblings("span.task-name").show();});
+    }
+    
+    function onEditTaskName($span) {
+        $span.hide().siblings("input.task-name").val($span.text()).show().focus();
+    }
+    
+    function onChangeTaskName($input) {
+        $input.hide();
+        var $span = $input.siblings("span.task-name");
+        if ($input.val()) {
+            $span.text($input.val());
+        }
+        $span.show();
     }
 }
 
